1fc66e820719acdce44f0b0822198ec627df1bec
Matt <mattblissmer@gmail.com>
2017-09-08 16:43:51 -0400

    first upload

---
+++ scenes/Game.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	if (Input.is_action_pressed("ui_cancel")):
+		get_tree().quit()
